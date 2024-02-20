367b530c70912647487817f300b3058095af647b
hanke <hanke0@outlook.com>
2024-02-20 12:55:11 +0800

    add action

--- release.sh
+++ release.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-ARCH='amd64'
+set -Eeo pipefail
 
-NAME=redis-xkeys
+release_platform() {
+    while [ $# -gt 0 ]; do
+        name="./dist/redis-xkeys-$1-$2"
+        if [ "$1" = "windows" ]; then
+            name="$name.exe"
+        fi
+        GOOS=$1 GOARCH=$2 go build -o "$name" .
+        shift 2
+    done
+}
 
-GOOS="linux" GOARCH=$ARCH go build -o $NAME-linux-$ARCH
-GOOS="darwin" GOARCH=$ARCH go build -o $NAME-darwin-$ARCH
-GOOS="windows" GOARCH=$ARCH go build -o $NAME-windows-$ARCH.exe
+rm -rf ./dist
+mkdir -p ./dist
+
+release_platform \
+    linux amd64 \
+    linux 386 \
+    windows amd64 \
+    windows 386 \
+    darwin arm64 \
+    darwin amd64
+
+cd ./dist
+md5sum >md5.sum ./*
